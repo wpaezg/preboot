00051e404627dff4a94bbc89929f8582f67f5c72
wpaezg <wpaezg@gmail.com>
2024-10-01 01:08:46 -0400

    Create bucle_for_basico1.py

---
+++ bucle_for_basico1.py
@@ -0,0 +1,21 @@
+for i in range(101):
+	print(i)
+for i in range(2,500,2):
+	print(i)
+for i in range(1,101):
+	if i % 10 ==0:
+	   print("baby")
+	elif i % 5 ==0:
+	   print("ice ice")
+	else:
+	   print(i)
+suma_total= sum(range(0,500001,2))
+print("Wow numero gigante a las vista:" , suma_total)
+for numero in range(2024,0,-3):
+    print(numero)
+numInicial=3
+numFinal=10
+multiplo=2
+for num in range(numInicial,numFinal):
+	if  num % multiplo ==0:
+	    print(num)
