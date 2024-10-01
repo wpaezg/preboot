b945fbf47fefbfbb9b9cc8a6e3b5f2cd25cc5ce1
wpaezg <wpaezg@gmail.com>
2024-09-30 21:55:34 -0400

    Create hola_mundo.py

---
+++ hola_mundo.py
@@ -0,0 +1,12 @@
+print ("Hola, mundo")
+nombre="Walter"
+print ("Hola,",nombre)
+print ("Hola, " + nombre)
+numero=11
+print("Hola",numero,"!")
+print("Hola " + str(numero))
+comida1="marineras"
+comida2="empanadas"
+print("Me encanta comer {} y {}.".format(comida1, comida2))
+print(f"Me encanta comer {comida1} y {comida2}")
+print("Me encanta comer %s y %s " %(comida1,comida2))
